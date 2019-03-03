a9427dd71c0b738ef743f6841c1b24e8cb5da869
moaaz-bhnas <moaaz_bs@yahoo.com>
2019-03-03 19:01:35 +0200

    set up for using js

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,15 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 sass.compiler = require('node-sass');
+const babel = require('gulp-babel');
  
 gulp.task('scripts', function() {
-  return gulp.src('./blocks/js/*.js')
+  return gulp.src('./blocks/**/*.js')
     .pipe(concat('all.js'))
-    .pipe(gulp.dest('./dist/js'));
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(gulp.dest('./js'));
 });
 
 gulp.task('sass', function () {
@@ -16,12 +20,13 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./css'));
 });
  
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+gulp.task('js:watch', function () {
+  gulp.watch('./blocks/**/*.js', gulp.series('scripts'));
 });
 
 // Gulp watch syntax
 gulp.task('watch', function(){
-    gulp.watch('./blocks/**/*.scss', gulp.series('sass')); 
+    gulp.watch('./blocks/**/*.scss', gulp.series('sass')),
+    gulp.watch('./blocks/**/*.js', gulp.series('scripts'));
     // Other watchers
   })
